03ab4f9d93338f7f712d2ab9546521740d722b1f
Justin Carper <jcarper@uplandsoftware.com>
2021-07-31 18:12:54 -0500

    make key look pressed from singalr message

--- SignalRPiano/wwwroot/js/piano.js
+++ SignalRPiano/wwwroot/js/piano.js
@@ -5,12 +5,16 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/midiHub").build();
 connection.on("ReceiveMessage", function (msg) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
+
     li.textContent = `${msg.note} at ${msg.velocity}`;
-    //document.getElementById("userInput").value = msg.userName;
-    //document.getElementById("messageInput").value = msg.message;
+
+    debugger;
+   //make the key look pressed
+    $(".key[data-key=" + msg.note + "]").addClass("pressed");
+    setTimeout(function () {
+        $(".key[data-key=" + msg.note + "]").removeClass("pressed");
+    }, 500);
+
 });
 
 connection.start().catch(function (err) {
@@ -18,19 +22,15 @@ connection.start().catch(function (err) {
 });
 
 $(".key").on("click", function () {
-    debugger;
     var mykey = this;
     //alert("Key:" + mykey.dataset.key);
-
-    //make the key look pressed
-    var jQueryObj = $(".key [data-key=" + mykey.dataset.key + "]");
-    jQueryObj.addClass("pressed");
-    setTimeout(function () {
-        jQueryObj.removeClass("pressed");
-    }, 500);
 
     connection.invoke("SendMessage", { "Note": mykey.dataset.key, "Velocity": 64 }).catch(function (err) {
         return console.error(err.toString());
     });
 
+});
+
+$("#playkey").on("click", function () {
+    $(".key[data-key=0]").addClass("pressed");
 });
